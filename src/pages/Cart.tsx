0580e3fcbe9d210730cdc0f3c6d8b5b16f423c45
sharmin2020331076 <sharminjuthi1080@gmail.com>
2025-07-10 18:09:24 +0600

    added Cart.tsx

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -5,8 +5,8 @@ import { CoffeeCard } from "../components/CoffeeCard";
 
 export function Cart(){
     return(
-        <div className="w-full min-h-screen">
-            <div className="pt-26 grid grid-cols-[60%_40%]">
+        <div className="w-full min-h-screen pt-20">
+            <div className="grid grid-cols-[60%_40%]">
             <div className="flex lg:flex-row flex-col md:space-x-12 space-x-0 space-y-32 lg:space-y-0 p-8">
         
 
@@ -15,7 +15,6 @@ export function Cart(){
                 description="Dark, sleek depth"
                 price="$3.50"
                 imageUrl="/icons/americano.png"
-                imageSize="w-60"
                  iconUrl={[
                     {
                     default: "/icons/heart.png",
@@ -33,7 +32,6 @@ export function Cart(){
                 description="Creamy and rich"
                 price="$3.50"
                 imageUrl="/icons/cup7.png"
-                imageSize="w-50"
                 iconUrl={[
                     {
                     default: "/icons/heart.png",
@@ -49,9 +47,9 @@ export function Cart(){
 
         </div>
 
-        <div className="p-8 space-y-4">
-            <h1 className="text-2xl font-bold text-coffee">Discover a world of flavor in every cup</h1>
-            <p>Savor the aroma that awakens your senses, crafted with care and passion for an unforgettable experience.</p>
+        <div className="p-8 space-y-6">
+            <h1 className="text-3xl font-bold text-coffee font-sans mb-10">Discover a world of flavor in every cup</h1>
+            <p className="text-xl font-semibold text-coffee font-sans">Savor the aroma that awakens your senses, crafted with care and passion for an unforgettable experience.</p>
             <Button text="Learn More" height="48px" width="128px" variant="gradient"/>
         </div>
         </div>
