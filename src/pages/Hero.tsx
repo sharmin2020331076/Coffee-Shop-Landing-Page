b01cebfce236080443d82bc520e1a15cbb82580b
sharmin2020331076 <sharminjuthi1080@gmail.com>
2026-01-29 23:42:45 +0600

    updated

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,7 +1,6 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import Button from "../components/Button";
-import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
+
 
 export function Hero(){
 
